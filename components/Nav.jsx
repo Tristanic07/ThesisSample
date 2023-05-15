fc3d0e450a511fc54c72fd8e1bde1554f8fed9e4
Tristanic07 <alainlado@gmail.com>
2023-05-15 21:33:20 +0800

    nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,13 @@
 const Nav = () => {
     return (
         <nav className="navs">
-            <h1>Thesis Modern Map</h1>
+            <h1 className="title">Thesis Modern Map</h1>
+            <ul className="navcontent">
+                <a href=""><li><h1 className="text">Home</h1></li></a>
+                <a href=""> <li><h1 className="text">About Us</h1></li></a>
+                <a href=""><li><h1 className="text">Contact Us</h1></li></a>
+                <a href=""> <li><h1 className="text">FaQ</h1></li></a>
+            </ul>
         </nav>
     )
 }
